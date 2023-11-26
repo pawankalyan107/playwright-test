27a9f49f55740c3620e28a65477a86728c579773
pawankalyan107 <vpawankalyan107@gmail.com>
2023-11-26 21:44:15 +0530

    updated to scrape

--- src/routes/routesV1.js
+++ src/routes/routesV1.js
@@ -2,25 +2,26 @@ import express from 'express'
 import { getArnDetails } from "../service/get-arn-details.js";
 
 const routesV1 = () => {
-    const router = new express.Router()
+  const router = new express.Router()
 
-    router.get(
-        '/arn/:id',
-        async (req, res) => {
-            try {
-                const _id = req.params.id
-                if (_id){
-                    const myArn = await getArnDetails(_id)
-                    res.status(200).send(myArn)
-                    console.log(myArn)
-                }
-            } catch (error) {
-                res.status(404).send()
-            }
+  router.get(
+    '/arn/:id',
+    (req, res) => {
+      try {
+        const _id = req.params.id
+        const cityname = `${_id}`.toUpperCase()
+        console.log(_id)
+        if (_id) {
+          getArnDetails(_id).then((resp)=> console.log('resp arrived'))
+          
+          res.send(`<h1>check at:::: ${cityname}.json</h1>`)
         }
-    )
-
-    return router
+      } catch (error) {
+        res.status(404).send()
+      }
+    }
+  )
+  return router
 }
 
 export default routesV1
--- src/service/get-arn-details.js
+++ src/service/get-arn-details.js
@@ -1,31 +1,67 @@
 import { chromium } from "playwright";
+import * as fs from 'fs';
+import * as path from 'path'
 
-export const getArnDetails = async (_id) => {
+export const getArnDetails = async (cityName) => {
   try {
     const browser = await chromium.launch();
     const page = await browser.newPage();
-    const element = page.locator('#txtARN');
-    const visitUrl = 'https://www.amfiindia.com/investor-corner/online-center/locate-mf-distributor.aspx';
+    const name = `${cityName}`.toUpperCase()
+    console.log('cityname:::', name)
 
+    const visitUrl = 'https://www.amfiindia.com/investor-corner/online-center/locate-mf-distributor.aspx';
     await page.goto(visitUrl);
-    await element.fill(`${_id}`);
+    const element = page.locator('#divCity').getByRole('textbox');
+ 
+    await element.fill(name)
+    await page.getByRole('banner').click()
     await page.getByRole('button', { name: 'Search' }).click().then(async () => {
       await page.waitForSelector('#pnlArn');
     });
-    const dataObject = {};
+
     const table = page.locator('.table-content');
     const rows = await table.locator('tr').all();
+    const len = rows.length;
+    console.log(len);
+    const writeStream = fs.createWriteStream(`${name}.json`, 'utf8');
+    writeStream.write('[');
+
     const keyCells = await rows[0].locator('td').all();
-    const valueCells = await rows[1].locator('td').all();
 
-    for (let i = 0; i < keyCells.length; i++) {
-      const key = await keyCells[i].textContent();
-      const value = await valueCells[i].textContent();
-      dataObject[key] = value;
+    const extractCellContents = async (row) => {
+      const valueCells = await row.locator('td').all();
+      const rowData = {};
+
+      await Promise.all(keyCells.map(async (keyCell, i) => {
+        const key = await keyCell.textContent();
+        const value = await valueCells[i].textContent();
+        rowData[key] = value;
+      }));
+
+      return rowData;
+    };
+
+    for (let j = 1; j < len; j++) {
+      const rowData = await extractCellContents(rows[j]);
+      const jsonData = JSON.stringify(rowData, null, 2);
+
+      writeStream.write(jsonData);
+      writeStream.write(',');
     }
-    await browser.close()
-    return dataObject
+    writeStream.write('{}')
+    writeStream.write(']');
+    writeStream.end();
 
+    writeStream.on('finish', () => {
+      console.log('Write stream finished');
+    });
+
+    writeStream.on('error', (error) => {
+      // Handle errors
+      console.error('Error:', error);
+    });
+    await browser.close()
+    return path.resolve(`${name}.json`)
   } catch (error) {
     throw error
   }
